4555aff0d8d831be0445b754921475366bfc80ed
guozhifeng.felton <guozhifeng.felton@bytedance.com>
2023-02-02 00:04:55 +0800

    feat(python): add python demo

---
+++ signature/sign.py
@@ -0,0 +1,159 @@
+"""
+@author zhouyiheng.go@bytedance.com
+@created 2022/07/29
+"""
+
+import datetime
+import hashlib
+import hmac
+import json
+from urllib.parse import quote
+
+import requests
+
+# 以下参数视服务不同而不同，一个服务内通常是一致的
+Service = "CDN"
+Version = "2021-03-01"
+Region = "cn-north-1"
+Host = "cdn.volcengineapi.com"
+ContentType = "application/json"
+
+# 请求的凭证，从IAM或者STS服务中获取
+AK = "AKLTExample***************"
+SK = "TmpBd056Rm1***************"
+# 当使用临时凭证时，需要使用到SessionToken传入Header，并计算进SignedHeader中，请自行在header参数中添加X-Security-Token头
+# SessionToken = ""
+
+
+def norm_query(params):
+    query = ""
+    for key in sorted(params.keys()):
+        if type(params[key]) == list:
+            for k in params[key]:
+                query = (
+                        query + quote(key, safe="-_.~") + "=" + quote(k, safe="-_.~") + "&"
+                )
+        else:
+            query = (query + quote(key, safe="-_.~") + "=" + quote(params[key], safe="-_.~") + "&")
+    query = query[:-1]
+    return query.replace("+", "%20")
+
+
+# 第一步：准备辅助函数。
+# sha256 非对称加密
+def hmac_sha256(key: bytes, content: str):
+    return hmac.new(key, content.encode("utf-8"), hashlib.sha256).digest()
+
+
+# sha256 hash算法
+def hash_sha256(content: str):
+    return hashlib.sha256(content.encode("utf-8")).hexdigest()
+
+
+# 第二步：创建一个 CDN 的 API 请求函数。签名计算的过程包含在该函数中。
+def request(method, date, query, header, ak, sk, action, body):
+    # 第三步：创建身份证明。其中的 Service 和 Region 字段是固定的。ak 和 sk 分别代表
+    # AccessKeyID 和 SecretAccessKey。同时需要初始化签名结构体。一些签名计算时需要的属性也在这里处理。
+    # 初始化身份证明结构体
+    credential = {
+        "access_key_id": ak,
+        "secret_access_key": sk,
+        "service": Service,
+        "region": Region,
+    }
+    # 初始化签名结构体
+    request_param = {
+        "body": body,
+        "host": Host,
+        "path": "/",
+        "method": method,
+        "content_type": ContentType,
+        "date": date,
+        "query": {"Action": action, "Version": Version, **query},
+    }
+    if body is None:
+        request_param["body"] = ""
+    # 第四步：接下来开始计算签名。在计算签名前，先准备好用于接收签算结果的 signResult 变量，并设置一些参数。
+    # 初始化签名结果的结构体
+    x_date = request_param["date"].strftime("%Y%m%dT%H%M%SZ")
+    short_x_date = x_date[:8]
+    x_content_sha256 = hash_sha256(request_param["body"])
+    sign_result = {
+        "Host": request_param["host"],
+        "X-Content-Sha256": x_content_sha256,
+        "X-Date": x_date,
+        "Content-Type": request_param["content_type"],
+    }
+    # 第五步：计算 Signature 签名。
+    signed_headers_str = ";".join(
+        ["content-type", "host", "x-content-sha256", "x-date"]
+    )
+    # signed_headers_str = signed_headers_str + ";x-security-token"
+    canonical_request_str = "\n".join(
+        [request_param["method"].upper(),
+         request_param["path"],
+         norm_query(request_param["query"]),
+         "\n".join(
+             [
+                 "content-type:" + request_param["content_type"],
+                 "host:" + request_param["host"],
+                 "x-content-sha256:" + x_content_sha256,
+                 "x-date:" + x_date,
+             ]
+         ),
+         "",
+         signed_headers_str,
+         x_content_sha256,
+         ]
+    )
+
+    # 打印正规化的请求用于调试比对
+    print(canonical_request_str)
+    hashed_canonical_request = hash_sha256(canonical_request_str)
+
+    # 打印hash值用于调试比对
+    print(hashed_canonical_request)
+    credential_scope = "/".join([short_x_date, credential["region"], credential["service"], "request"])
+    string_to_sign = "\n".join(["HMAC-SHA256", x_date, credential_scope, hashed_canonical_request])
+
+    # 打印最终计算的签名字符串用于调试比对
+    print(string_to_sign)
+    k_date = hmac_sha256(credential["secret_access_key"].encode("utf-8"), short_x_date)
+    k_region = hmac_sha256(k_date, credential["region"])
+    k_service = hmac_sha256(k_region, credential["service"])
+    k_signing = hmac_sha256(k_service, "request")
+    signature = hmac_sha256(k_signing, string_to_sign).hex()
+
+    sign_result["Authorization"] = "HMAC-SHA256 Credential={}, SignedHeaders={}, Signature={}".format(
+        credential["access_key_id"] + "/" + credential_scope,
+        signed_headers_str,
+        signature,
+    )
+    header = {**header, **sign_result}
+    # header = {**header, **{"X-Security-Token": SessionToken}}
+    # 第六步：将 Signature 签名写入 HTTP Header 中，并发送 HTTP 请求。
+    r = requests.request(method=method,
+                         url="https://{}{}".format(request_param["host"], request_param["path"]),
+                         headers=header,
+                         params=request_param["query"],
+                         data=request_param["body"],
+                         )
+    return r.json()
+
+
+if __name__ == "__main__":
+    # response_body = request("Get", datetime.datetime.utcnow(), {}, {}, AK, SK, "ListUsers", None)
+    # print(response_body)
+
+    now = datetime.datetime.utcnow()
+    request_body = {
+        "Interval": "5min",
+        "Metric": "bandwidth",
+        "StartTime": int((now - datetime.timedelta(minutes=5)).timestamp()),
+        "EndTime": int(now.timestamp()),
+        "Domain": "example.com",
+    }
+
+    # Body的格式需要配合Content-Type，API使用的类型请阅读具体的官方文档
+    response_body = request("POST", now, {}, {}, AK, SK, "DescribeCdnData", json.dumps(request_body))
+    print(response_body)
