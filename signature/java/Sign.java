269713fcb1d32a0a7f6f2f093a746bd2e9f3182a
guozhifeng.felton <guozhifeng.felton@bytedance.com>
2023-03-02 03:17:35 +0800

    feat(add demo): add or modify multi language demo

---
+++ signature/java/Sign.java
@@ -0,0 +1,231 @@
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * Copyright (year) Beijing Volcano Engine Technology Ltd.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class Sign {
+
+    private static final BitSet URLENCODER = new BitSet(256);
+
+    private static final String CONST_ENCODE = "0123456789ABCDEF";
+    public static final Charset UTF_8 = StandardCharsets.UTF_8;
+
+    private final String region;
+    private final String service;
+    private final String schema;
+    private final String host;
+    private final String path;
+    private final String ak;
+    private final String sk;
+
+    static {
+        int i;
+        for (i = 97; i <= 122; ++i) {
+            URLENCODER.set(i);
+        }
+
+        for (i = 65; i <= 90; ++i) {
+            URLENCODER.set(i);
+        }
+
+        for (i = 48; i <= 57; ++i) {
+            URLENCODER.set(i);
+        }
+        URLENCODER.set('-');
+        URLENCODER.set('_');
+        URLENCODER.set('.');
+        URLENCODER.set('~');
+    }
+
+    public Sign(String region, String service, String schema, String host, String path, String ak, String sk) {
+        this.region = region;
+        this.service = service;
+        this.host = host;
+        this.schema = schema;
+        this.path = path;
+        this.ak = ak;
+        this.sk = sk;
+    }
+
+    public static void main(String[] args) throws Exception {
+        String SecretAccessKey = "****";
+        String AccessKeyID = "AK****";
+        // 请求地址
+        String endpoint = "iam.volcengineapi.com";
+        String path = "/"; // 路径，不包含 Query// 请求接口信息
+        String service = "iam";
+        String region = "cn-beijing";
+        String schema = "https";
+        Sign sign = new Sign(region, service, schema, endpoint, path, AccessKeyID, SecretAccessKey);
+
+        String action = "ListPolicies";
+        String version = "2018-01-01";
+
+        Date date = new Date();
+        HashMap<String, String> queryMap = new HashMap<>() {{
+            put("Limit", "1");
+        }};
+
+
+        sign.doRequest("POST", queryMap, null, date, action, version);
+    }
+
+    public void doRequest(String method, Map<String, String> queryList, byte[] body,
+                          Date date, String action, String version) throws Exception {
+        if (body == null) {
+            body = new byte[0];
+        }
+        String xContentSha256 = hashSHA256(body);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd'T'HHmmss'Z'");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        String xDate = sdf.format(date);
+        String shortXDate = xDate.substring(0, 8);
+        String contentType = "application/x-www-form-urlencoded";
+
+        String signHeader = "host;x-date;x-content-sha256;content-type";
+
+
+        SortedMap<String, String> realQueryList = new TreeMap<>(queryList);
+        realQueryList.put("Action", action);
+        realQueryList.put("Version", version);
+        StringBuilder querySB = new StringBuilder();
+        for (String key : realQueryList.keySet()) {
+            querySB.append(signStringEncoder(key)).append("=").append(signStringEncoder(realQueryList.get(key))).append("&");
+        }
+        querySB.deleteCharAt(querySB.length() - 1);
+
+        String canonicalStringBuilder = method + "\n" + path + "\n" + querySB + "\n" +
+                "host:" + host + "\n" +
+                "x-date:" + xDate + "\n" +
+                "x-content-sha256:" + xContentSha256 + "\n" +
+                "content-type:" + contentType + "\n" +
+                "\n" +
+                signHeader + "\n" +
+                xContentSha256;
+
+        System.out.println(canonicalStringBuilder);
+
+        String hashcanonicalString = hashSHA256(canonicalStringBuilder.getBytes());
+        String credentialScope = shortXDate + "/" + region + "/" + service + "/request";
+        String signString = "HMAC-SHA256" + "\n" + xDate + "\n" + credentialScope + "\n" + hashcanonicalString;
+
+        byte[] signKey = genSigningSecretKeyV4(sk, shortXDate, region, service);
+        String signature = HexFormat.of().formatHex(hmacSHA256(signKey, signString));
+
+
+        URL url = new URL(schema + "://" + host + path + "?" + querySB);
+
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod(method);
+        conn.setRequestProperty("Host", host);
+        conn.setRequestProperty("X-Date", xDate);
+        conn.setRequestProperty("X-Content-Sha256", xContentSha256);
+        conn.setRequestProperty("Content-Type", contentType);
+        conn.setRequestProperty("Authorization", "HMAC-SHA256" +
+                " Credential=" + ak + "/" + credentialScope +
+                ", SignedHeaders=" + signHeader +
+                ", Signature=" + signature);
+        if (!Objects.equals(conn.getRequestMethod(), "GET")) {
+            conn.setDoOutput(true);
+            OutputStream os = conn.getOutputStream();
+            os.write(body);
+            os.flush();
+            os.close();
+        }
+        conn.connect();
+
+        int responseCode = conn.getResponseCode();
+
+        InputStream is;
+        if (responseCode == 200) {
+            is = conn.getInputStream();
+        } else {
+            is = conn.getErrorStream();
+        }
+        String responseBody = new String(is.readAllBytes());
+        is.close();
+
+        System.out.println(responseCode);
+        System.out.println(responseBody);
+    }
+
+    private String signStringEncoder(String source) {
+        if (source == null) {
+            return null;
+        }
+        StringBuilder buf = new StringBuilder(source.length());
+        ByteBuffer bb = UTF_8.encode(source);
+        while (bb.hasRemaining()) {
+            int b = bb.get() & 255;
+            if (URLENCODER.get(b)) {
+                buf.append((char) b);
+            } else if (b == 32) {
+                buf.append("%20");
+            } else {
+                buf.append("%");
+                char hex1 = CONST_ENCODE.charAt(b >> 4);
+                char hex2 = CONST_ENCODE.charAt(b & 15);
+                buf.append(hex1);
+                buf.append(hex2);
+            }
+        }
+
+        return buf.toString();
+    }
+
+    public static String hashSHA256(byte[] content) throws Exception {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+            return HexFormat.of().formatHex(md.digest(content));
+        } catch (Exception e) {
+            throw new Exception(
+                    "Unable to compute hash while signing request: "
+                            + e.getMessage(), e);
+        }
+    }
+
+    public static byte[] hmacSHA256(byte[] key, String content) throws Exception {
+        try {
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(new SecretKeySpec(key, "HmacSHA256"));
+            return mac.doFinal(content.getBytes());
+        } catch (Exception e) {
+            throw new Exception(
+                    "Unable to calculate a request signature: "
+                            + e.getMessage(), e);
+        }
+    }
+
+    private byte[] genSigningSecretKeyV4(String secretKey, String date, String region, String service) throws Exception {
+        byte[] kDate = hmacSHA256((secretKey).getBytes(), date);
+        byte[] kRegion = hmacSHA256(kDate, region);
+        byte[] kService = hmacSHA256(kRegion, service);
+        return hmacSHA256(kService, "request");
+    }
+}
---
+++ signature/php/sign.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Copyright (year) Beijing Volcano Engine Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+require 'vendor/autoload.php';
+
+// 需要自行安装 composer（https://getcomposer.org/doc/00-intro.md），并安装GuzzleHttp依赖， composer require guzzlehttp/guzzle:^7.0
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+
+$Service = "iam";
+$Version = "2018-01-01";
+$Region = "cn-north-1";
+$Host = "iam.volcengineapi.com";
+$ContentType = "application/x-www-form-urlencoded";
+/**
+ * @throws GuzzleException
+ */
+// 第一步：创建一个  API 请求函数。签名计算的过程包含在该函数中。
+function request($method, $query, $header, $ak, $sk, $action, $body)
+{
+
+    // 第二步：创建身份证明。其中的 Service 和 Region 字段是固定的。ak 和 sk 分别代表
+    // AccessKeyID 和 SecretAccessKey。同时需要初始化签名结构体。一些签名计算时需要的属性也在这里处理。
+    // 初始化身份证明结构体
+    global $Service, $Region, $Host, $Version, $ContentType;
+    $credential = [
+        'accessKeyId' => $ak,
+        'secretKeyId' => $sk,
+        'service' => $Service,
+        'region' => $Region,
+    ];
+
+    // 初始化签名结构体
+    $requestParam = [
+        // body是http请求需要的原生body
+        'body' => $body,
+        'host' => $Host,
+        'path' => '/',
+        'method' => $method,
+        'contentType' => $ContentType,
+        'date' => gmdate('Ymd\THis\Z'),
+        'query' => array_merge($query, [
+            'Action' => $action,
+            'Version' => $Version
+        ])
+    ];
+
+    // 第三步：接下来开始计算签名。在计算签名前，先准备好用于接收签算结果的 signResult 变量，并设置一些参数。
+    // 初始化签名结果的结构体
+    $xDate = $requestParam['date'];
+    $shortXDate = substr($xDate, 0, 8);
+    $xContentSha256 = hash('sha256', $requestParam['body']);
+    $signResult = [
+        'Host' => $requestParam['host'],
+        'X-Content-Sha256' => $xContentSha256,
+        'X-Date' => $xDate,
+        'Content-Type' => $requestParam['contentType']
+    ];
+    // 第四步：计算 Signature 签名。
+    $signedHeaderStr = join(';', ['content-type', 'host', 'x-content-sha256', 'x-date']);
+    $canonicalRequestStr = join("\n", [
+        $requestParam['method'],
+        $requestParam['path'],
+        http_build_query($requestParam['query']),
+        join("\n", ['content-type:' . $requestParam['contentType'], 'host:' . $requestParam['host'], 'x-content-sha256:' . $xContentSha256, 'x-date:' . $xDate]),
+        '',
+        $signedHeaderStr,
+        $xContentSha256
+    ]);
+    $hashedCanonicalRequest = hash("sha256", $canonicalRequestStr);
+    $credentialScope = join('/', [$shortXDate, $credential['region'], $credential['service'], 'request']);
+    $stringToSign = join("\n", ['HMAC-SHA256', $xDate, $credentialScope, $hashedCanonicalRequest]);
+    $kDate = hash_hmac("sha256", $shortXDate, $credential['secretKeyId'], true);
+    $kRegion = hash_hmac("sha256", $credential['region'], $kDate, true);
+    $kService = hash_hmac("sha256", $credential['service'], $kRegion, true);
+    $kSigning = hash_hmac("sha256", 'request', $kService, true);
+    $signature = hash_hmac("sha256", $stringToSign, $kSigning);
+    $signResult['Authorization'] = sprintf("HMAC-SHA256 Credential=%s, SignedHeaders=%s, Signature=%s", $credential['accessKeyId'] . '/' . $credentialScope, $signedHeaderStr, $signature);
+    $header = array_merge($header, $signResult);
+    // 第五步：将 Signature 签名写入 HTTP Header 中，并发送 HTTP 请求。
+    $client = new Client([
+        'base_uri' => 'https://' . $requestParam['host'],
+        'timeout' => 120.0,
+    ]);
+    return $client->request($method, 'https://' . $requestParam['host'] . $requestParam['path'], [
+        'headers' => $header,
+        'query' => $requestParam['query'],
+        'body' => $requestParam['body']
+    ]);
+}
+
+$now = time();
+
+$requestBody ="";
+
+$AK = 'ak';
+$SK = 'sk';
+
+try {
+    $response = request("GET", [], [], $AK, $SK, "ListUsers", $requestBody);
+    print_r($response->getBody()->getContents());
+} catch (GuzzleException $e) {
+    print_r($e->getMessage());
+}
